85d74e2f35adf3a609347d74547224790ad3034d
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-09-28 16:56:19 +0300

    new

--- quiz/Magazine.py
+++ quiz/Magazine.py
@@ -4,3 +4,9 @@ class Magazine(Item):
     def __init__(self, name, borrowIn, borrowed=False , author="", cover=""):
         super().__init__(name, borrowIn, borrowed)
         self.cover = cover
+
+    def displayInfo(self):
+        print(f"Magazine Name: {self.name}")
+        print(f"Price: {self.price}")
+        print(f"Borrowed: {self.borrowed}")
+        return self
