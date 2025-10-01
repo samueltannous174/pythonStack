ff3232ba305db0e4cf2b432f2bf43dcf6a2772e2
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-10-01 11:57:29 +0300

    new

---
+++ I-django/I-django_fundamentals/intro/intro/app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+def root(request):
+    return HttpResponseRedirect('/blogs')
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return HttpResponseRedirect('/')
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return HttpResponseRedirect('/blogs')
+
