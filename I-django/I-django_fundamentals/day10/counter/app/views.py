f16a5ce2719957e5ab9660140dda8432012f4cb1
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-10-05 21:54:25 +0300

    new

--- I-django/I-django_fundamentals/day10/counter/app/views.py
+++ I-django/I-django_fundamentals/day10/counter/app/views.py
@@ -3,12 +3,12 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request):
     
-    return render(request, 'app/index.html', {'counter': request.session.get('counter', 0), 'visit_count': get_visit_count(request.session)})
+    return render(request, 'app/index.html', {'counter': request.session.get('counter', 0), 'visit_count': get_visit_count_and_inc(request.session)})
 
 def counter(request):
     counter_value = request.session.get('counter', 0) + 1
     request.session['counter'] = counter_value
-    return render(request, 'app/index.html', {'counter': counter_value, 'visit_count': get_visit_count(request.session)})
+    return render(request, 'app/index.html', {'counter': counter_value, 'visit_count': get_visit_count_and_inc(request.session)})
 
 def reset(request):
     request.session.flush()
@@ -22,6 +22,11 @@ def increment_by_2(request):
     return render(request, 'app/index.html', {'counter': counter_value, 'visit_count': get_visit_count(request.session)})
 
 def get_visit_count(session) :
+    visit_count = session.get('visit_count', 0)
+    session['visit_count'] = visit_count
+    return visit_count
+
+def get_visit_count_and_inc(session) :
     visit_count = session.get('visit_count', 0)
     visit_count += 1
     session['visit_count'] = visit_count
